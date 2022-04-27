70488e537f844bff9a4a33b4e8a65d59903b1291
kathrynzaro <zarokathryn@gmail.com>
2022-04-27 11:01:23 -0700

    completed exercise saving for reference

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const colors = ['red', 'blue', 'green', 'yellow']
 
 console.log("============ Question 1 ============")
 // Using its index value in colors, console.log 'blue'
+console.log(colors[1]);
 
 console.log("============ Question 2 ============")
+colors.push('purple');
+console.log(colors)
 // add the value 'purple' to the end of the colors array
 // console.log the result
 
@@ -15,17 +18,23 @@ const benny = {
 
 console.log("============ Question 3 ============")
 // Using dot notation, console.log the name and age of Benny
-
+console.log(benny.name);
+console.log(benny.age);
 console.log("============ Question 4 ============")
 // Using dot notation, console.log any one of Benny's breeds
+console.log(benny.breeds[0]);
+for(let breed of benny.breeds){
+  console.log(breed);
+}
 
 console.log("============ Question 5 ============")
 // Add code to the whisper function so that it takes whatever string is given to it
 // and returns an all lower case version
 
 function whisper(word){
-  // Your code here
+  return word.toLowerCase();
 }
+
 
 console.log(`this should print true: ${whisper('Hello') === 'hello'}`)
 console.log(`this should print true: ${whisper('BOOGERS') === 'boogers'}`)
@@ -50,10 +59,24 @@ const dogs = [{
 }]
 console.log("============ BONUS -- 1 ============")
 // Using index values and dot notation, console.log each dog's name
+console.log(dogs[0].name)
+console.log(dogs[1].name)
+console.log(dogs[2].name)
 
 console.log("============ BONUS -- 2 ============")
 // Using index values and dot notation, console.log the first breed of each dog
+// console.log(dogs[0].breeds[0]);
+// console.log(dogs[1].breeds[0]);
+// console.log(dogs[2].breeds[0]);
+
+for (let dog of dogs) {
+  console.log(dog.name, dog.age);
+  for (let breed of dog.breeds) {
+    console.log(breed);
+  }
+}
 
 console.log("============ BONUS -- 3 ============")
 // Using template literals, print the following string
 // Benny is 6 years old
+console.log(`${dogs[0].name} is ${dogs[0].age} years old`)
